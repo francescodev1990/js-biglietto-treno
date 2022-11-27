b29b9f9b95c6324d84f27749dd963da8c57f6f12
Your Name <almannofrancesco@gmail.com>
2022-11-27 22:46:50 +0100

    if else added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,28 @@ console.log(chilometri);
 let età = parseInt(prompt('Per favore adesso inserisci la tua età, servirà per calcolare il prezzo finale.'))
 console.log(età);
 
+//CALCOLO PREZZO BIGLIETTO//
+
+let price_ticket = (0,21);
+let num_km = (20);
+
+let total_price = (price_ticket * num_km);
+console.log(total_price);
+
+//CONDIZIONI//
+
+if(età < 18){
+    console.log('Il cliente avrà uno sconto del 20%');
+}
+else if(età > 65){
+    console.log('Il cliente avrà uno sconto del 40%')
+}
+else{
+    console.log('Il cliente non avrà nessuno sconto')
+}
+
+//SCONTI PER CATEGORIE//
+
+let sconto_1 = total_price * 20 / 100;
+
+let sconto_2 = total_price * 40 / 100;

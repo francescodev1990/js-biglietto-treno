fe5b4c95f5d6182c528870392f5c3df04efd2a14
Your Name <almannofrancesco@gmail.com>
2022-11-27 11:35:23 +0100

    parseInt added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-let chilometri = prompt('Ciao! Per favore inserisci il numero dei km che vorresti percorrere');
+//CHILOMETRI PERCORSI//
+
+let chilometri = parseInt(prompt('Ciao! Per favore inserisci il numero dei km che vorresti percorrere.'))
 console.log(chilometri);
+
+//ETA' UTENTE//
+let età = parseInt(prompt('Per favore adesso inserisci la tua età, servirà per calcolare il prezzo finale.'))
+console.log(età);
+

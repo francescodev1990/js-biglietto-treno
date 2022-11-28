ffb5000c5b8c7e51c02d41efcd79f07685b56c94
Your Name <almannofrancesco@gmail.com>
2022-11-28 15:48:14 +0100

    condizioni e sconti

--- js/script.js
+++ js/script.js
@@ -1,42 +1,56 @@
-//CHILOMETRI PERCORSI//
+//RICHIESTA DEI CHILOMETRI PERCORSI//
 
 let chilometri = parseInt(prompt('Ciao! Per favore inserisci il numero dei km che vorresti percorrere.'))
 console.log(chilometri);
 
-//ETA' UTENTE//
+//RICHIESTA DELL'ETA' DEL CLIENTE//
 let eta = parseInt(prompt('Per favore adesso inserisci la tua età, servirà per calcolare il prezzo finale.'))
 console.log(eta);
 
 //CALCOLO PREZZO BIGLIETTO//
 
-let price_ticket = (0,21);
-let num_km = (20);
+let price_ticket = 0.21 * chilometri;
+console.log(price_ticket);
 
-let total_price = (price_ticket * num_km);
+let total_price = (price_ticket * chilometri);
 console.log(total_price);
 
 //CONDIZIONI//
+//SCONTI//
 
 if(eta < 18){
-    console.log('Il cliente avrà uno sconto del 20%');
     let sconto_1 = total_price * 20 / 100;
     total_price = total_price - sconto_1;
-    console.log(sconto_1);
+    console.log('Il cliente avrà uno sconto del 20%');
+    
 }
 else if(eta > 65){
-    console.log('Il cliente avrà uno sconto del 40%')
     let sconto_2 = total_price * 40 / 100;
     total_price = total_price - sconto_2;
+    console.log('Il cliente avrà uno sconto del 40%')
 }
 else{
     console.log('Il cliente non avrà nessuno sconto')
 }
 
-//SCONTI PER CATEGORIE//
 
-let sconto_1 = (total_price * 20) / 100;
-console.log(sconto_1);
 
-let sconto_2 = (total_price * 40) / 100;
-console.log(sconto_2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
